a950f61ba5382a9a7ffaa1c3c52f06d8901ccc4b
cherr0 <adsl1664@gmail.com>
2022-09-24 23:22:34 +0900

    Feature: Library 사용 구성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,39 @@
-import '../styles/globals.css'
+import '../styles/_variables.css'
 
+import {
+    Hydrate,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+                retry: 3,
+            },
+        },
+    })
+    return (
+        <>
+            <Head>
+                <title>OKKY - All that developer</title>
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <RecoilRoot>
+                        <Component {...pageProps} />
+                    </RecoilRoot>
+                </Hydrate>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </>
+    )
 }
 
 export default MyApp
